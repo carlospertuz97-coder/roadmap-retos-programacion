b26ae6f28d99bbed2481e75536fac683d21805ba
carlospertuz97-coder <carlospertuz.97@gmail.com>
2025-10-17 12:16:50 -0500

    Create carlospertuz97-coder.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/carlospertuz97-coder.py
@@ -0,0 +1,12 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+esto tambien es
+un comentario
+en varias lineas
+"""
+my_variable = " mi variable"
+my_variable = "nuevo valor de mi variable"
+
